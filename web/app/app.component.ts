907c33374ad8d60b46696574753e6eb344e6742e
Justin Cheung <jygsaw@gmail.com>
2016-06-03 20:35:37 -0700

    [NEW] Implement mock slow hero fetch

--- web/app/app.component.ts
+++ web/app/app.component.ts
@@ -78,7 +78,7 @@ export class AppComponent implements OnInit {
   constructor(private heroService: HeroService) { };
 
   getHeroes() {
-    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+    this.heroService.getHeroesSlowly().then(heroes => this.heroes = heroes);
   }
 
   ngOnInit() {
