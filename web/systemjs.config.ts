8b1df435e7c3d0a74167f4f2bdd06c478f636f9d
Justin Cheung <jygsaw@gmail.com>
2016-06-08 11:02:39 -0700

    [CHG] Replace heroes mock data with in-memory backend

--- web/systemjs.config.ts
+++ web/systemjs.config.ts
@@ -11,7 +11,7 @@
   let packages = {
     'app':                        { main: 'main.js',  defaultExtension: 'js' },
     'rxjs':                       { defaultExtension: 'js' },
-    'angular2-in-memory-web-api': { defaultExtension: 'js' },
+    'angular2-in-memory-web-api': { main: 'index.js', defaultExtension: 'js' },
   };
   let packageNames = [
     '@angular/common',
